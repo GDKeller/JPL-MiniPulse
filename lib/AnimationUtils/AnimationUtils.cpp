9e304a5b5ec0782ab5337f8a5854ef3c696dd945
Grant Keller <grant@kellerdigital.com>
2022-11-10 00:00:06 -0800

    Basic AnimationUtils class definition

---
+++ lib/AnimationUtils/AnimationUtils.cpp
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include <AnimationUtils.h>
+
+int AnimationUtils::getBrightness() {
+  return 255;
+}
---
+++ lib/AnimationUtils/AnimationUtils.h
@@ -0,0 +1,16 @@
+/*
+    Animation Utilities
+    Created by Grant Keller, November 2022
+*/
+
+#include <Arduino.h>
+
+#ifndef AnimationUtils_h
+#define AnimationUtils_h
+
+class AnimationUtils {
+    public:
+        int getBrightness();
+};
+
+#endif
