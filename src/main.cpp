6ade83f874b50924d256ffd8bf1679143ffbff02
Grant Keller <grant@kellerdigital.com>
2023-02-04 03:22:41 -0800

    Moved WiFi reset check out of main loop

--- src/main.cpp
+++ src/main.cpp
@@ -248,6 +248,39 @@ void printMeteorArray() {
 	Serial.println(printString);
 }
 
+// Force Wifi portal when WiFi reset button is pressed
+void checkWifiButton(){
+  // check for button press
+  if ( digitalRead(WIFI_RST) == LOW ) {
+    // poor mans debounce/press-hold to prevent accidental trigger
+    delay(50);
+    if( digitalRead(WIFI_RST) == LOW ){
+      Serial.println("WiFi Reset button pressed");
+      // still holding button for 3000 ms, reset settings
+      delay(3000); // reset delay hold
+      if( digitalRead(WIFI_RST) == LOW ){
+        Serial.println("Button Held");
+        Serial.println("Erasing Config, restarting");
+        wm.resetSettings();
+        ESP.restart();
+      }
+      
+      // start portal w delay
+      Serial.println("Starting config portal");
+      wm.setConfigPortalTimeout(120);
+      
+      if (!wm.startConfigPortal(AP_SSID)) {
+        Serial.println("failed to connect or hit timeout");
+        delay(3000);
+        // ESP.restart();
+      } else {
+        //if you get here you have connected to the WiFi
+        Serial.println("Connected to WiFi)");
+      }
+    }
+  }
+}
+
 
 
 int allStripsLength = sizeof(allStrips) / sizeof(allStrips[0]);
@@ -293,108 +326,108 @@ uint32_t brightnessAdjust(uint32_t color)
 	return Adafruit_NeoPixel::Color(((AnimationUtils::brightness * channelR) / 255), ((AnimationUtils::brightness * channelG) / 255), ((AnimationUtils::brightness * channelB) / 255));
 }
 
-// Fill strip pixels one after another with a color. Strip is NOT cleared
-// first; anything there will be covered pixel by pixel. Pass in color
-// (as a single 'packed' 32-bit value, which you can get by calling
-// strip.Color(red, green, blue) as shown in the loop() function above),
-// and a delay time (in milliseconds) between pixels.
-void colorWipe(Adafruit_NeoPixel &strip, uint32_t *color, int wait)
-{
-	for (int i = 0; i < strip.numPixels(); i++)
-	{									   // For each pixel in strip...
-		au.setPixelColor(strip, i, color); //  Set pixel's color (in RAM)
-		strip.show();					   //  Update strip to match
-		delay(wait);					   //  Pause for a moment
-	}
-}
+// // Fill strip pixels one after another with a color. Strip is NOT cleared
+// // first; anything there will be covered pixel by pixel. Pass in color
+// // (as a single 'packed' 32-bit value, which you can get by calling
+// // strip.Color(red, green, blue) as shown in the loop() function above),
+// // and a delay time (in milliseconds) between pixels.
+// void colorWipe(Adafruit_NeoPixel &strip, uint32_t *color, int wait)
+// {
+// 	for (int i = 0; i < strip.numPixels(); i++)
+// 	{									   // For each pixel in strip...
+// 		au.setPixelColor(strip, i, color); //  Set pixel's color (in RAM)
+// 		strip.show();					   //  Update strip to match
+// 		delay(wait);					   //  Pause for a moment
+// 	}
+// }
 
-// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
-// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
-// between frames.
-void theaterChase(Adafruit_NeoPixel &strip, uint32_t *color, int wait)
-{
-	for (int a = 0; a < 10; a++)
-	{ // Repeat 10 times...
-		for (int b = 0; b < 3; b++)
-		{				   //  'b' counts from 0 to 2...
-			strip.clear(); //   Set all pixels in RAM to 0 (off)
+// // Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// // a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// // between frames.
+// void theaterChase(Adafruit_NeoPixel &strip, uint32_t *color, int wait)
+// {
+// 	for (int a = 0; a < 10; a++)
+// 	{ // Repeat 10 times...
+// 		for (int b = 0; b < 3; b++)
+// 		{				   //  'b' counts from 0 to 2...
+// 			strip.clear(); //   Set all pixels in RAM to 0 (off)
 
-			// 'c' counts up from 'b' to end of strip in steps of 3...
-			for (int c = b; c < strip.numPixels(); c += 3)
-			{
-				au.setPixelColor(strip, c, color); // Set pixel 'c' to value 'color'
-			}
-			strip.show(); // Update strip with new contents
-			delay(wait);  // Pause for a moment
-		}
-	}
-}
+// 			// 'c' counts up from 'b' to end of strip in steps of 3...
+// 			for (int c = b; c < strip.numPixels(); c += 3)
+// 			{
+// 				au.setPixelColor(strip, c, color); // Set pixel 'c' to value 'color'
+// 			}
+// 			strip.show(); // Update strip with new contents
+// 			delay(wait);  // Pause for a moment
+// 		}
+// 	}
+// }
 
-// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(Adafruit_NeoPixel &strip, int wait)
-{
-	// Hue of first pixel runs 5 complete loops through the color wheel.
-	// Color wheel has a range of 65536 but it's OK if we roll over, so
-	// just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-	// means we'll make 5*65536/256 = 1280 passes through this loop:
-	for (long firstPixelHue = 0; firstPixelHue < 1 * 65536; firstPixelHue += 256)
-	{
-		// strip.rainbow() can take a single argument (first pixel hue) or
-		// optionally a few extras: number of rainbow repetitions (default 1),
-		// saturation and value (brightness) (both 0-255, similar to the
-		// ColorHSV() function, default 255), and a true/false flag for whether
-		// to apply gamma correction to provide 'truer' colors (default true).
-		au.updateBrightness();
-		strip.rainbow(firstPixelHue, 1, 255, AnimationUtils::brightness, true);
-		// Above line is equivalent to:
-		// strip.rainbow(firstPixelHue, 1, 255, 255, true);
-		strip.show(); // Update strip with new contents
-		delay(wait);  // Pause for a moment
-	}
-}
+// // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+// void rainbow(Adafruit_NeoPixel &strip, int wait)
+// {
+// 	// Hue of first pixel runs 5 complete loops through the color wheel.
+// 	// Color wheel has a range of 65536 but it's OK if we roll over, so
+// 	// just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+// 	// means we'll make 5*65536/256 = 1280 passes through this loop:
+// 	for (long firstPixelHue = 0; firstPixelHue < 1 * 65536; firstPixelHue += 256)
+// 	{
+// 		// strip.rainbow() can take a single argument (first pixel hue) or
+// 		// optionally a few extras: number of rainbow repetitions (default 1),
+// 		// saturation and value (brightness) (both 0-255, similar to the
+// 		// ColorHSV() function, default 255), and a true/false flag for whether
+// 		// to apply gamma correction to provide 'truer' colors (default true).
+// 		au.updateBrightness();
+// 		strip.rainbow(firstPixelHue, 1, 255, AnimationUtils::brightness, true);
+// 		// Above line is equivalent to:
+// 		// strip.rainbow(firstPixelHue, 1, 255, 255, true);
+// 		strip.show(); // Update strip with new contents
+// 		delay(wait);  // Pause for a moment
+// 	}
+// }
 
-// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void hueCycle(Adafruit_NeoPixel &strip, int wait)
-{
-	// Hue of first pixel runs 5 complete loops through the color wheel.
-	// Color wheel has a range of 65536 but it's OK if we roll over, so
-	// just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-	// means we'll make 5*65536/256 = 1280 passes through this loop:
-	for (long hue = 0; hue < 1 * 65536; hue += 256)
-	{
-		au.updateBrightness();
-		strip.fill(Adafruit_NeoPixel::gamma32(Adafruit_NeoPixel::ColorHSV(hue, 255, AnimationUtils::brightness)));
-		strip.show(); // Update strip with new contents
-		delay(wait);  // Pause for a moment
-	}
-}
+// // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+// void hueCycle(Adafruit_NeoPixel &strip, int wait)
+// {
+// 	// Hue of first pixel runs 5 complete loops through the color wheel.
+// 	// Color wheel has a range of 65536 but it's OK if we roll over, so
+// 	// just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+// 	// means we'll make 5*65536/256 = 1280 passes through this loop:
+// 	for (long hue = 0; hue < 1 * 65536; hue += 256)
+// 	{
+// 		au.updateBrightness();
+// 		strip.fill(Adafruit_NeoPixel::gamma32(Adafruit_NeoPixel::ColorHSV(hue, 255, AnimationUtils::brightness)));
+// 		strip.show(); // Update strip with new contents
+// 		delay(wait);  // Pause for a moment
+// 	}
+// }
 
-// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
-void theaterChaseRainbow(Adafruit_NeoPixel &strip, int wait)
-{
-	int firstPixelHue = 0; // First pixel starts at red (hue 0)
-	for (int a = 0; a < 30; a++)
-	{ // Repeat 30 times...
-		for (int b = 0; b < 3; b++)
-		{				   //  'b' counts from 0 to 2...
-			strip.clear(); //   Set all pixels in RAM to 0 (off)
-			// 'c' counts up from 'b' to end of strip in increments of 3...
-			for (int c = b; c < strip.numPixels(); c += 3)
-			{
-				// hue of pixel 'c' is offset by an amount to make one full
-				// revolution of the color wheel (range 65536) along the length
-				// of the strip (strip.numPixels() steps):
-				int hue = firstPixelHue + c * 65536L / strip.numPixels();
-				uint32_t color = strip.ColorHSV(hue); // hue -> RGB
-				const uint32_t *pColor = &color;
-				au.setPixelColor(strip, c, pColor); // Set pixel 'c' to value 'color'
-			}
-			strip.show();				 // Update strip with new contents
-			delay(wait);				 // Pause for a moment
-			firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
-		}
-	}
-}
+// // Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+// void theaterChaseRainbow(Adafruit_NeoPixel &strip, int wait)
+// {
+// 	int firstPixelHue = 0; // First pixel starts at red (hue 0)
+// 	for (int a = 0; a < 30; a++)
+// 	{ // Repeat 30 times...
+// 		for (int b = 0; b < 3; b++)
+// 		{				   //  'b' counts from 0 to 2...
+// 			strip.clear(); //   Set all pixels in RAM to 0 (off)
+// 			// 'c' counts up from 'b' to end of strip in increments of 3...
+// 			for (int c = b; c < strip.numPixels(); c += 3)
+// 			{
+// 				// hue of pixel 'c' is offset by an amount to make one full
+// 				// revolution of the color wheel (range 65536) along the length
+// 				// of the strip (strip.numPixels() steps):
+// 				int hue = firstPixelHue + c * 65536L / strip.numPixels();
+// 				uint32_t color = strip.ColorHSV(hue); // hue -> RGB
+// 				const uint32_t *pColor = &color;
+// 				au.setPixelColor(strip, c, pColor); // Set pixel 'c' to value 'color'
+// 			}
+// 			strip.show();				 // Update strip with new contents
+// 			delay(wait);				 // Pause for a moment
+// 			firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+// 		}
+// 	}
+// }
 
 
 
@@ -1159,6 +1192,7 @@ void getData(void *parameter)
 
 	for (;;)
 	{
+		checkWifiButton();
 
 		// Send an HTTP POST request every 5 seconds
 		if ((millis() - lastTime) > timerDelay) {
@@ -1170,39 +1204,6 @@ void getData(void *parameter)
 	}
 }
 
-
-// Force Wifi portal when WiFi reset button is pressed
-void checkWifiButton(){
-  // check for button press
-  if ( digitalRead(WIFI_RST) == LOW ) {
-    // poor mans debounce/press-hold to prevent accidental trigger
-    delay(50);
-    if( digitalRead(WIFI_RST) == LOW ){
-      Serial.println("WiFi Reset button pressed");
-      // still holding button for 3000 ms, reset settings
-      delay(3000); // reset delay hold
-      if( digitalRead(WIFI_RST) == LOW ){
-        Serial.println("Button Held");
-        Serial.println("Erasing Config, restarting");
-        wm.resetSettings();
-        ESP.restart();
-      }
-      
-      // start portal w delay
-      Serial.println("Starting config portal");
-      wm.setConfigPortalTimeout(120);
-      
-      if (!wm.startConfigPortal(AP_SSID)) {
-        Serial.println("failed to connect or hit timeout");
-        delay(3000);
-        // ESP.restart();
-      } else {
-        //if you get here you have connected to the WiFi
-        Serial.println("Connected to WiFi)");
-      }
-    }
-  }
-}
 
 // setup() function -- runs once at startup --------------------------------
 void setup()
@@ -1404,8 +1405,10 @@ void setup()
 
 	if (queue == NULL)
 	{ // Check that queue was created successfully
-		Serial.println("Error creating the queue");
-		delay(3000);
+		#if SHOW_SERIAL == 1
+			Serial.println("Error creating the queue");
+			delay(3000);
+		#endif
 		ESP.restart();
 	}
 
@@ -1417,7 +1420,6 @@ void setup()
 // loop() function -- runs repeatedly as long as board is on ---------------
 void loop()
 {
-	checkWifiButton();
 
 	if (TEST_CORES == 1)
 	{
@@ -1545,7 +1547,7 @@ void loop()
 		// perfDiff = (millis() - perfTimer);	// This is the actual value
 		UBaseType_t uxHighWaterMark;
 		uxHighWaterMark = uxTaskGetStackHighWaterMark( xHandleData );
-		printFreeHeap();	// Value is being multiplied in printFreeHeap() function for ease of visualization on plotter
+		printFreeHeap();	// Value might be being multiplied in printFreeHeap() function for ease of visualization on plotter
 		// Serial.print("high_water_mark:"); Serial.print(uxHighWaterMark); Serial.print(",");
 		Serial.print("ParseCounter:"); Serial.print(parseCounter * 1000); Serial.print(",");	// Multiplied by 10 for ease of visualization on plotter
 		// Serial.print("ParseCounter:"); Serial.print(parseCounter); Serial.print(",");	// This is the actual value
