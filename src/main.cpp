9d1f690b7db3a2ae9db2f90df927bb27a9b545a1
Grant Keller <grant@kellerdigital.com>
2023-10-31 06:41:48 -0700

    Change dummy xml

--- src/main.cpp
+++ src/main.cpp
@@ -211,7 +211,7 @@ const char* data_font_test = PROGMEM R"==--==(<?xml version='1.0' encoding='utf-
 </dsn>)==--==";
 
 // Set the data to use
-const char* dummyXmlData = data_animation_test;
+const char* dummyXmlData = data_Sept6;
 
 
 /* STATE TRACKING
